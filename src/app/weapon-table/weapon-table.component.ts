8439df775377ffef870c14a61ed7bd80c8f3c7af
SoHectic <jimmywright@cogeco.ca>
2023-02-15 17:37:53 -0500

    I changed alot

--- src/app/weapon-table/weapon-table.component.ts
+++ src/app/weapon-table/weapon-table.component.ts
@@ -9,41 +9,54 @@ export interface WeaponData{
   limbDmg: number;
   bodyDmg: number;
   headDmg: number;
-  magCap: string[];
-  btk: string[];
+  magCap: string;
+  btk: string;
+  RPM: number;
+  DPS: number;
+  dmgPerMagBody: number;
+  dmgPerMagHead: number;
 }
 
 const ELEMENT_DATA: WeaponData[] = [
-  {category: 'Assault Rifle',name: 'R-301',ammoType: 'Light', limbDmg: 10, bodyDmg: 13, headDmg: 21, magCap: ["No Mag: 18", "Gray: 20", "Blue: 25", "Purple/Gold: 28"], btk: ["None: 8", "Gray: 12", "Blue: 14", "Purple: 16", "Gold: 16", "Red: 18"]},
-  {category: 'Assault Rifle',name: 'VK-47 Flatline',ammoType: 'Heavy', limbDmg: 14, bodyDmg: 18, headDmg: 29, magCap: ["No Mag: 20", "Gray: 25", "Blue: 28", "Purple/Gold: 30"], btk: ["None: 6", "Gray: 9", "Blue: 10", "Purple: 12", "Gold: 12", "Red: 13"]},
-  {category: 'Assault Rifle',name: 'Hemlock',ammoType: 'Heavy', limbDmg: 17, bodyDmg: 23, headDmg: 38, magCap: ["Base: 30"], btk: ["None: 5", "Gray: 7", "Blue: 8", "Purple: 9", "Gold: 9", "Red: 10"]},
-  {category: 'Assault Rifle',name: 'Nemesis',ammoType: 'Energy', limbDmg: 13, bodyDmg: 17, headDmg: 27, magCap: ["No Mag: 20", "Gray: 24", "Blue: 28", "Purple/Gold: 32"], btk: ["None: 6", "Gray: 9", "Blue: 11", "Purple: 12", "Gold: 12", "Red: 14"]},
-  {category: 'Assault Rifle',name: 'Havoc',ammoType: 'Energy', limbDmg: 14, bodyDmg: 18, headDmg: 29, magCap: ["No Mag: 24", "Gray: 28", "Blue: 32", "Purple/Gold: 36"], btk: ["None: 6", "Gray: 9", "Blue: 10", "Purple: 12", "Gold: 12", "Red: 13"]},
-  {category: 'Sub Machine Gun',name: 'Alternator',ammoType: 'Light', limbDmg: 13, bodyDmg: 16, headDmg: 22, magCap: ["No Mag: 19", "Gray: 22", "Blue: 25", "Purple/Gold: 27"], btk: ["None: 7", "Gray: 10", "Blue: 11", "Purple: 13", "Gold: 13", "Red: 15"]},
-  {category: 'Sub Machine Gun',name: 'R-99',ammoType: 'Light', limbDmg: 10, bodyDmg: 12, headDmg: 17, magCap: ["No Mag: 20", "Gray: 22", "Blue: 25", "Purple/Gold: 28"], btk: ["None: 9", "Gray: 13", "Blue: 15", "Purple: 17", "Gold: 17", "Red: 19"]},
-  {category: 'Sub Machine Gun',name: 'Car',ammoType: 'Light / Heavy', limbDmg: 10, bodyDmg: 13, headDmg: 18, magCap: ["No Mag: 19", "Gray: 21", "Blue: 23", "Purple/Gold: 26"], btk: ["None: 8", "Gray: 12", "Blue: 14", "Purple: 16", "Gold: 16", "Red: 18"]},
-  {category: 'Sub Machine Gun',name: 'Prowler',ammoType: 'Heavy', limbDmg: 12, bodyDmg: 15, headDmg: 21, magCap: ["No Mag: 20", "Gray: 25", "Blue: 30", "Purple/Gold: 35"], btk: ["None: 7", "Gray: 10", "Blue: 12", "Purple: 14", "Gold: 14", "Red: 15"]},
-  {category: 'Sub Machine Gun',name: 'Volt',ammoType: 'Energy', limbDmg: 12, bodyDmg: 15, headDmg: 21, magCap: ["No Mag: 19", "Gray: 21", "Blue: 23", "Purple/Gold: 26"], btk: ["None: 7", "Gray: 10", "Blue: 12", "Purple: 14", "Gold: 14", "Red: 15"]},
-  {category: 'Light Machine Gun',name: 'Devotion',ammoType: 'Energy', limbDmg: 13, bodyDmg: 15, headDmg: 21, magCap: ["No Mag: 36", "Gray: 40", "Blue: 44", "Purple/Gold: 48"], btk: ["None: 7", "Gray: 10", "Blue: 12", "Purple: 14", "Gold: 14", "Red: 15"]},
-  {category: 'Light Machine Gun',name: 'L-Star',ammoType: 'Energy', limbDmg: 14, bodyDmg: 17, headDmg: 24, magCap: ["Overheats  "], btk: ["None: 6", "Gray: 9", "Blue: 11", "Purple: 12", "Gold: 12", "Red: 14"]},
-  {category: 'Light Machine Gun',name: 'Spitfire',ammoType: 'Light', limbDmg: 15, bodyDmg: 18, headDmg: 25, magCap: ["No Mag: 35", "Gray: 40", "Blue: 45", "Purple/Gold: 50"], btk: ["None: 6", "Gray: 9", "Blue: 10", "Purple: 12", "Gold: 12", "Red: 13"]},
-  {category: 'Light Machine Gun',name: 'Rampage',ammoType: 'Heavy', limbDmg: 22, bodyDmg: 26, headDmg: 36, magCap: ["No Mag: 28", "Gray: 32", "Blue: 34", "Purple/Gold: 40"], btk: ["None: 4", "Gray: 6", "Blue: 7", "Purple:8 ", "Gold: 8", "Red: 9"]},
-  {category: 'Marksman Rifle',name: 'G7 Scout',ammoType: 'Light', limbDmg: 24, bodyDmg: 32, headDmg: 50, magCap: ["No Mag: 10", "Gray: 15", "Blue: 18", "Purple/Gold: 20"], btk: ["None: 4", "Gray: 5", "Blue: 6", "Purple: 7", "Gold: 7", "Red: 8"]},
-  {category: 'Marksman Rifle',name: 'Triple Take',ammoType: 'Energy', limbDmg: 19, bodyDmg: 21, headDmg: 34, magCap: ["No Mag: 18", "Gray: 21", "Blue: 24", "Purple/Gold: 27"], btk: ["None: 5", "Gray: 8", "Blue: 9", "Purple: 10", "Gold: 10", "Red: 11"]},
-  {category: 'Marksman Rifle',name: '30-30*',ammoType: 'Heavy', limbDmg: 48, bodyDmg: 57, headDmg: 91, magCap: ["No Mag: 6", "Gray: 8", "Blue: 10", "Purple/Gold: 12"], btk: ["None: 2", "Gray: 3", "Blue: 4", "Purple: 4", "Gold: 4", "Red: 4"]},
-  {category: 'Marksman Rifle',name: 'Bocek',ammoType: 'Air Drop', limbDmg: 63, bodyDmg: 70, headDmg: 112, magCap: ["Base: 1"], btk: ["None: 2", "Gray: 3", "Blue: 3", "Purple: 3", "Gold: 3", "Red: 4"]},
-  {category: 'Sniper Rifle',name: 'Charge Rifle',ammoType: 'Sniper', limbDmg: 90, bodyDmg: 90, headDmg: 114, magCap: ["Base: 8"], btk: ["None: 2", "Gray: 2", "Blue: 2", "Purple: 3", "Gold: 3", "Red: 3"]},
-  {category: 'Sniper Rifle',name: 'Sentinel',ammoType: 'Sniper', limbDmg: 63, bodyDmg: 70, headDmg: 126, magCap: ["No Mag: 4", "Gray: 5", "Blue: 6", "Purple/Gold: 7"], btk: ["None: 2", "Gray: 3", "Blue: 3", "Purple: 3", "Gold: 3", "Red: 4"]},
-  {category: 'Sniper Rifle',name: 'Longbow',ammoType: 'Sniper', limbDmg: 44, bodyDmg: 55, headDmg: 106, magCap: ["No Mag: 6", "Gray: 8", "Blue: 10", "Purple/Gold: 12"], btk: ["None: 2", "Gray: 3", "Blue: 4", "Purple: 4", "Gold: 4", "Red: 5"]},
-  {category: 'Sniper Rifle',name: 'Kraber',ammoType: 'Air Drop', limbDmg: 112, bodyDmg: 140, headDmg: 252, magCap: ["Base: 4"], btk: ["None: 1", "Gray: 2", "Blue: 2", "Purple: 2", "Gold: 2", "Red: 2"]},
-  {category: 'Shotgun',name: 'Eva-8',ammoType: 'Shotgun', limbDmg: 7, bodyDmg: 7, headDmg: 8, magCap: ["Base: 8"], btk: ["None: 15", "Gray: 22", "Blue: 25", "Purple: 29", "Gold: 29", "Red: 33"]},
-  {category: 'Shotgun',name: 'Mastiff',ammoType: 'Shotgun', limbDmg: 11, bodyDmg: 11, headDmg: 13, magCap: ["Base: 5"], btk: ["None: 10", "Gray: 14", "Blue: 16", "Purple: 19", "Gold: 19", "Red: 21"]},
-  {category: 'Shotgun',name: 'Mozambique',ammoType: 'Shotgun', limbDmg: 15, bodyDmg: 15, headDmg: 18, magCap: ["Base: 6"], btk: ["None: 7", "Gray: 10", "Blue: 12", "Purple: 14", "Gold: 14", "Red: 15"]},
-  {category: 'Shotgun',name: 'Peacekeeper',ammoType: 'Shotgun', limbDmg: 9, bodyDmg: 9, headDmg: 11, magCap: ["Base: 5"], btk: ["None: 12", "Gray: 17", "Blue: 20", "Purple: 23", "Gold: 23", "Red: 25"]},
-  {category: 'Pistol',name: 'RE-45',ammoType: 'Air Drop', limbDmg: 13, bodyDmg: 14, headDmg: 20, magCap: ["Base: 25"], btk: ["None: 8", "Gray: 11", "Blue: 13", "Purple: 15", "Gold: 15", "Red: 17"]},
-  {category: 'Pistol',name: 'P2020',ammoType: 'Light', limbDmg: 16, bodyDmg: 18, headDmg: 25, magCap: ["No Mag: 14", "Gray: 16", "Blue: 18", "Purple/Gold: 21"], btk: ["None: 6", "Gray: 9", "Blue: 10", "Purple: 12", "Gold: 12", "Red: 14"]},
-  {category: 'Pistol',name: 'Wingman',ammoType: 'Sniper', limbDmg: 41, bodyDmg: 45, headDmg: 86, magCap: ["No Mag: 6", "Gray: 7", "Blue: 8", "Purple/Gold: 9"], btk: ["None: 3", "Gray: 4", "Blue: 4", "Purple: 5", "Gold: 5", "Red: 5"]},
-  {category: 'Special',name: 'Throwing Knife',ammoType: 'Air Drop', limbDmg: 100, bodyDmg: 100, headDmg: 260, magCap: ["Base: 1"], btk: ["None: 1", "Gray: 2", "Blue: 2", "Purple: 2", "Gold: 2", "Red: 3"]},
+  {category: 'Assault Rifle',name: 'R-301',ammoType: 'Light', limbDmg: 10, bodyDmg: 13, headDmg: 21, magCap: "18/20/25/28", btk: "12/14/16/18", RPM: 810, DPS: 176, dmgPerMagBody: 364, dmgPerMagHead: 588},
+  {category: 'Assault Rifle',name: 'VK-47 Flatline',ammoType: 'Heavy', limbDmg: 14, bodyDmg: 18, headDmg: 29, magCap: "20/25/28/30", btk: "9/10/12/13", RPM: 600, DPS: 180, dmgPerMagBody: 540, dmgPerMagHead: 870},
+  {category: 'Assault Rifle',name: 'Hemlock',ammoType: 'Heavy', limbDmg: 17, bodyDmg: 23, headDmg: 38, magCap: "30", btk: "7/8/9/10", RPM: 414, DPS: 159, dmgPerMagBody: 690, dmgPerMagHead: 1140},
+  {category: 'Assault Rifle',name: 'Nemesis',ammoType: 'Energy', limbDmg: 13, bodyDmg: 17, headDmg: 27, magCap: "20/24/28/32", btk: "9/11/12/14", RPM: 667, DPS: 189, dmgPerMagBody: 544, dmgPerMagHead: 864},
+  {category: 'Assault Rifle',name: 'Havoc',ammoType: 'Energy', limbDmg: 14, bodyDmg: 18, headDmg: 29, magCap: "24/28/32/36", btk: "9/10/12/13", RPM: 672, DPS: 202, dmgPerMagBody: 648, dmgPerMagHead: 1044},
+  {category: 'Assault Rifle',name: 'Havoc Turbo',ammoType: 'Energy', limbDmg: 13, bodyDmg: 17, headDmg: 27, magCap: "24/28/32/36", btk: "9/10/12/13", RPM: 672, DPS: 202, dmgPerMagBody: 612, dmgPerMagHead: 972},
+  {category: 'SMG',name: 'Alternator',ammoType: 'Light', limbDmg: 13, bodyDmg: 16, headDmg: 22, magCap: "19/22/25/27", btk: "10/11/13/15", RPM: 600, DPS: 160, dmgPerMagBody: 432, dmgPerMagHead: 594},
+  {category: 'SMG',name: 'R-99',ammoType: 'Light', limbDmg: 10, bodyDmg: 12, headDmg: 17, magCap: "20/22/25/28", btk: "13/15/17/19", RPM: 1080, DPS: 216, dmgPerMagBody: 336, dmgPerMagHead: 476},
+  {category: 'SMG',name: 'Car',ammoType: 'Light / Heavy', limbDmg: 10, bodyDmg: 13, headDmg: 18, magCap: "19/21/23/26", btk: "12/14/16/18", RPM: 930, DPS: 202, dmgPerMagBody: 338, dmgPerMagHead: 468},
+  {category: 'SMG',name: 'Prowler',ammoType: 'Heavy', limbDmg: 12, bodyDmg: 15, headDmg: 21, magCap: "20/25/30/35", btk: "10/12/14/15", RPM: 667, DPS: 167, dmgPerMagBody: 525, dmgPerMagHead: 735},
+  {category: 'SMG',name: 'Volt',ammoType: 'Energy', limbDmg: 12, bodyDmg: 15, headDmg: 21, magCap: "19/21/23/26", btk: "10/12/14/15", RPM: 720, DPS: 180, dmgPerMagBody: 390,dmgPerMagHead: 546},
+  {category: 'LMG',name: 'Devotion',ammoType: 'Energy', limbDmg: 13, bodyDmg: 15, headDmg: 21, magCap: "36/40/44/48", btk: "10/12/14/15", RPM: 900, DPS: 225, dmgPerMagBody: 720, dmgPerMagHead: 1008},
+  {category: 'LMG',name: 'L-Star*',ammoType: 'Energy', limbDmg: 14, bodyDmg: 17, headDmg: 24, magCap: "Overheats", btk: "9/11/12/14", RPM: 600, DPS: 170, dmgPerMagBody: 510, dmgPerMagHead: 750},
+  {category: 'LMG',name: 'Spitfire',ammoType: 'Light', limbDmg: 15, bodyDmg: 18, headDmg: 25, magCap: "35/40/45/50", btk: "9/10/12/13", RPM: 540, DPS: 162, dmgPerMagBody: 900,dmgPerMagHead: 1250},
+  {category: 'LMG',name: 'Rampage Revved Up',ammoType: 'Heavy', limbDmg: 22, bodyDmg: 26, headDmg: 36, magCap: "28/32/34/40", btk: "6/7/8/9", RPM: 390, DPS: 169, dmgPerMagBody: 1040,dmgPerMagHead: 1440},
+  {category: 'LMG',name: 'Rampage',ammoType: 'Heavy', limbDmg: 22, bodyDmg: 26, headDmg: 36, magCap: "28/32/34/40", btk: "6/7/8/9", RPM: 300, DPS: 130, dmgPerMagBody: 1040,dmgPerMagHead: 1440},
+  {category: 'Marksman',name: 'G7 Scout',ammoType: 'Light', limbDmg: 24, bodyDmg: 32, headDmg: 50, magCap: "10/15/18/20", btk: "5/6/7/8", RPM: 240, DPS: 128, dmgPerMagBody: 640,dmgPerMagHead: 1000},
+  {category: 'Marksman',name: 'Triple Take',ammoType: 'Energy', limbDmg: 57, bodyDmg: 63, headDmg: 102, magCap: "18/21/24/27", btk: "3/3/4/4", RPM: 81, DPS: 85, dmgPerMagBody: 567,dmgPerMagHead: 918},
+  {category: 'Marksman',name: '30-30',ammoType: 'Heavy', limbDmg: 36, bodyDmg: 42, headDmg: 67, magCap: "6/8/10/12", btk: "4/5/5/6", RPM: 139, DPS: 97, dmgPerMagBody: 504,dmgPerMagHead: 804},
+  {category: 'Marksman',name: '30-30 Charged',ammoType: 'Heavy', limbDmg: 48, bodyDmg: 57, headDmg: 91, magCap: "6/8/10/12", btk: "3/4/4/4", RPM: 139, DPS: 97, dmgPerMagBody: 684,dmgPerMagHead: 1092},
+  {category: 'Marksman',name: '30-30 SP',ammoType: 'Heavy', limbDmg: 36, bodyDmg: 42, headDmg: 79, magCap: "6/8/10/12", btk: "4/5/5/6", RPM: 139, DPS: 97, dmgPerMagBody: 504,dmgPerMagHead: 948},
+  {category: 'Marksman',name: '30-30 Charged SP',ammoType: 'Heavy', limbDmg: 48, bodyDmg: 57, headDmg: 107, magCap: "6/8/10/12", btk: "3/4/4/4", RPM: 139, DPS: 97, dmgPerMagBody: 684,dmgPerMagHead: 1284},
+  {category: 'Marksman',name: 'Bocek Shatter Caps',ammoType: 'Air Drop', limbDmg: 63, bodyDmg: 70, headDmg: 112, magCap: "1", btk: "3/3/3/4", RPM: 180, DPS: 129, dmgPerMagBody: 70,dmgPerMagHead: 112},
+  {category: 'Marksman',name: 'Bocek',ammoType: 'Air Drop', limbDmg: 63, bodyDmg: 70, headDmg: 112, magCap: "1", btk: "3/3/3/4", RPM: 180, DPS: 107, dmgPerMagBody: 70,dmgPerMagHead: 112},
+  {category: 'Sniper',name: 'Charge Rifle',ammoType: 'Sniper', limbDmg: 90, bodyDmg: 90, headDmg: 114, magCap: "8", btk: "2/2/3/3", RPM: 26, DPS: 39, dmgPerMagBody: 360,dmgPerMagHead: 456},
+  {category: 'Sniper',name: 'Sentinel',ammoType: 'Sniper', limbDmg: 63, bodyDmg: 70, headDmg: 126, magCap: "4/5/6/7", btk: "3/3/3/4", RPM: 38, DPS: 44, dmgPerMagBody: 490,dmgPerMagHead: 882},
+  {category: 'Sniper',name: 'Sentinel Amped',ammoType: 'Sniper', limbDmg: 79, bodyDmg: 88, headDmg: 158, magCap: "4/5/6/7", btk: "2/2/3/3", RPM: 38, DPS: 58, dmgPerMagBody: 616,dmgPerMagHead: 1106},
+  {category: 'Sniper',name: 'Longbow',ammoType: 'Sniper', limbDmg: 44, bodyDmg: 55, headDmg: 106, magCap: "6/8/10/12", btk: "3/4/4/5", RPM: 78, DPS: 72, dmgPerMagBody: 660,dmgPerMagHead: 1272},
+  {category: 'Sniper',name: 'Longbow SP',ammoType: 'Sniper', limbDmg: 44, bodyDmg: 55, headDmg: 121, magCap: "6/8/10/12", btk: "3/4/4/5", RPM: 78, DPS: 72, dmgPerMagBody: 660,dmgPerMagHead: 1452},
+  {category: 'Sniper',name: 'Kraber',ammoType: 'Air Drop', limbDmg: 112, bodyDmg: 140, headDmg: 252, magCap: "4", btk: "2/2/2/2", RPM: 25, DPS: 58, dmgPerMagBody: 560,dmgPerMagHead: 1008},
+  {category: 'Shotgun',name: 'Eva-8',ammoType: 'Shotgun', limbDmg: 56, bodyDmg: 56, headDmg: 64, magCap: "8", btk: "3/4/4/5", RPM: 138, DPS: 129, dmgPerMagBody: 448,dmgPerMagHead: 512},
+  {category: 'Shotgun',name: 'Mastiff',ammoType: 'Shotgun', limbDmg: 88, bodyDmg: 88, headDmg: 104, magCap: "5", btk: "2/2/3/3", RPM: 66, DPS: 97, dmgPerMagBody: 440,dmgPerMagHead: 520},
+  {category: 'Shotgun',name: 'Mozambique',ammoType: 'Shotgun', limbDmg: 45, bodyDmg: 45, headDmg: 54, magCap: "6", btk: "4/4/5/5", RPM: 132, DPS: 99, dmgPerMagBody: 270,dmgPerMagHead: 324},
+  {category: 'Shotgun',name: 'Peacekeeper',ammoType: 'Shotgun', limbDmg: 99, bodyDmg: 99, headDmg: 121, magCap: "5", btk: "2/2/2/3", RPM: 44, DPS: 73, dmgPerMagBody: 495,dmgPerMagHead: 605},
+  {category: 'Pistol',name: 'RE-45 Disruptor',ammoType: 'Air Drop', limbDmg: 16, bodyDmg: 18, headDmg: 26, magCap: "25", btk: "11/13/15/17", RPM: 780, DPS: 234, dmgPerMagBody: 450,dmgPerMagHead: 650},
+  {category: 'Pistol',name: 'RE-45',ammoType: 'Light', limbDmg: 13, bodyDmg: 14, headDmg: 20, magCap: "25", btk: "11/13/15/17", RPM: 780, DPS: 182, dmgPerMagBody: 350,dmgPerMagHead: 500},
+  {category: 'Pistol',name: 'P2020',ammoType: 'Light', limbDmg: 16, bodyDmg: 18, headDmg: 25, magCap: "14/16/18/21", btk: "9/10/12/14", RPM: 420, DPS: 126, dmgPerMagBody: 588,dmgPerMagHead: 525},
+  {category: 'Pistol',name: 'Wingman',ammoType: 'Sniper', limbDmg: 41, bodyDmg: 45, headDmg: 86, magCap: "6/7/8/9", btk: "4/4/5/5", RPM: 156, DPS: 117, dmgPerMagBody: 405,dmgPerMagHead: 774},
+  {category: 'Pistol',name: 'Wingman SP',ammoType: 'Sniper', limbDmg: 41, bodyDmg: 45, headDmg: 99, magCap: "6/7/8/9", btk: "4/4/5/5", RPM: 156, DPS: 117, dmgPerMagBody: 405,dmgPerMagHead: 891}
 ];
 
 @Component({
@@ -60,7 +73,11 @@ export class WeaponTableComponent implements AfterViewInit{
     'bodyDmg',
     'headDmg',
     'magCap',
-    'btk'
+    'btk',
+    'RPM',
+    'DPS',
+    'dmgPerMagBody',
+    'dmgPerMagHead'
   ];
 
   dataSource = new MatTableDataSource(ELEMENT_DATA);
